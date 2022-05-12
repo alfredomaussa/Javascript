edb81e2453d943cb03e53a99f6e7e465a429d4ca
A4lfr32 <alfredomaussa@gmail.com>
2022-05-12 17:10:53 -0500

    Solved

--- Day0/Codewar_0.js
+++ Day0/Codewar_0.js
@@ -1,10 +1,10 @@
 var summation = function (num) {
   // Code here
-  let element;
-  for (let index = 0; index < num; index++) {
+  let element = 0;
+  for (let index = 1; index < num + 1; index++) {
     element += index;
   }
   return element;
-}
+};
 
 console.log(summation(4));
